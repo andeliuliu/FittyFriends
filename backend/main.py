c91641599e6a7f254a48936358fd4dc9c4e9a839
kubarozwadowski <officialkubarozwadowski@gmail.com>
2024-09-15 09:23:58 -0400

    user inputted local data

--- backend/main.py
+++ backend/main.py
@@ -7,11 +7,10 @@ from terra.base_client import Terra
 import requests
 from datetime import datetime
 from bson import ObjectId
-from schemas import list_serial, individual_serial
-import db
-from db import pets_collection
 from pets import Pet
-from route import router
+from pets import router as pets_router
+from typing import List
+import json
 
 
 # Load environment variables
@@ -22,9 +21,11 @@ DEV_ID = os.getenv('DEV_ID')
 SECRET = os.getenv('SECRET')
 terra = Terra(API_KEY, DEV_ID, SECRET)
 
+PETS_FILE = "pets_data.json"
+
 # Initialize FastAPI app
 app = FastAPI()
-app.include_router(router)
+app.include_router(pets_router)
 
 def get_headers():
     return {
@@ -35,7 +36,7 @@ def get_headers():
 
 def fetch_daily_data(user_id: str):
     today = datetime.now().strftime('%Y-%m-%d')  # Get today's date in 'YYYY-MM-DD' format
-    url = f"https://api.tryterra.co/v2/daily?user_id={user_id}&start_date={'2024-09-01'}&end_date={'2024-09-01'}&to_webhook=false"
+    url = f"https://api.tryterra.co/v2/daily?user_id={user_id}&start_date={today}&end_date={today}&to_webhook=false"
     
     headers = get_headers()  # Get headers with API Key and Dev ID
     response = requests.get(url, headers=headers)  # Make GET request to Terra API
@@ -49,6 +50,7 @@ def fetch_daily_data(user_id: str):
 def filter_data(daily_data: dict):
     data_entry = daily_data[0]
     return {
+        "date": datetime.now().strftime('%Y-%m-%d'),
         "steps": data_entry.get("distance_data", {}).get("steps", 0),
         "total_burned_calories": data_entry.get("calories_data", {}).get("total_burned_calories", 0),
         "avg_hr_bpm": round(data_entry.get("heart_rate_data", {}).get("summary", {}).get("avg_hr_bpm", 0), 2),   
